934d54e5f7e768edd759d8bf90eb755183c696f2
Weverton Icaro <weverton.dev@gmail.com>
2022-09-13 09:14:05 -0300

    install cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -26,7 +26,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/car", express.static(`${upload.tmpFolder}/car`));
 
-app.use(cors())
+app.use(cors());
 app.use(router);
 
 app.use(
